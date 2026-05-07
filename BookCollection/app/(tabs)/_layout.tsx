c537b90379f9217bd4ea8442cca7ed3172136270
fritoure1 <abdel.elfassi07@hotmail.com>
2026-05-07 16:00:12 +0200

    improve color theme change

--- BookCollection/app/(tabs)/_layout.tsx
+++ BookCollection/app/(tabs)/_layout.tsx
@@ -1,11 +1,27 @@
 import { Tabs } from 'expo-router';
+import { useColorScheme } from '@/hooks/use-color-scheme';
+import { Colors } from '@/constants/theme';
 
 export default function TabLayout() {
+  const theme = useColorScheme() ?? 'light';
+
   return (
-    <Tabs screenOptions={{ tabBarActiveTintColor: '#10b981' }}>
+    <Tabs screenOptions={{ 
+        tabBarActiveTintColor: '#10b981',
+        tabBarInactiveTintColor: theme === 'dark' ? '#9BA1A6' : '#687076',
+        tabBarStyle: {
+            backgroundColor: Colors[theme].background, 
+            borderTopColor: theme === 'dark' ? '#333' : '#eee',
+        },
+        headerStyle: {
+            backgroundColor: Colors[theme].background, 
+        },
+        headerTintColor: Colors[theme].text,
+    }}>
       <Tabs.Screen name="index" options={{ title: "Ma Biblio" }} />
       <Tabs.Screen name="scanner" options={{ title: "Scanner" }} />
       <Tabs.Screen name="stats" options={{ title: "Stats" }} />
     </Tabs>
   );
 }
+
